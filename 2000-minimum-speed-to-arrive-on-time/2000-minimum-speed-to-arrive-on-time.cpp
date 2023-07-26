05f9837cfdf4cbca5ae31defbbbddf86da0bc343
Gyanvi <84316427+Gyanvi908@users.noreply.github.com>
2023-07-26 22:22:19 +0530

    Time: 321 ms (68.06%), Space: 101.5 MB (26.33%) - LeetHub

---
+++ 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int n;
+    double time(vector<int>& dist, int speed){
+        double sum=0;
+        for(int i=0; i<n-1; i++)
+            sum+=(dist[i]+speed-1)/speed;//ceiling function
+        sum+=(double)dist[n-1]/speed;// last one just normal division
+        cout<<"speed="<<speed<<",time="<<sum<<endl;
+        return sum;
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        n=dist.size();
+       
+        if (hour<=n-1) return -1;
+        long long left=1;
+        int right=1+max(*max_element(dist.begin(), dist.end()), int(dist[n-1]/(hour-n+1)));
+    //    cout<<right<<endl;
+        int speed;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if (time(dist, mid)<=hour){
+                speed=mid;
+                right=mid-1;
+            } 
+            else left=mid+1;
+        }
+        return speed;
+    }
+};
